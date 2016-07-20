90cdf796e750e813ceeb7abe7548d9354b254620
Marco Mancuso <marco.mancuso@alfresco.com>
2016-07-20 14:43:37 +0100

    https://github.com/sinfomicien/mysql2_chef_gem/issues/14

--- metadata.rb
+++ metadata.rb
@@ -26,7 +26,8 @@ depends "java", ">= 1.31.0"
 depends "libreoffice", ">= 0.0.2"
 depends 'line', '>= 0.6.2'
 depends "logstash-forwarder", '>= 0.1.1'
-depends "mysql", ">= 6.0.21"
+#depends "mysql", ">= 6.0.21"
+depends 'mysql', '~> 7.2'
 depends 'mysql2_chef_gem', ">= 1.0.1"
 depends "nginx", ">= 2.7.6"
 depends "openssl", ">= 4.0.0"
